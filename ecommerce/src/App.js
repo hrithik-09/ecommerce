dcd8ac4950d175cc24263bc257cba3433cb4991a
Hrithik <ranjan.hrithik09@gmail.com>
2023-03-31 16:05:15 +0530

    Header design completed

---
+++ ecommerce/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import {Browser, Routes,Route, BrowserRouter} from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+function App() {
+  return (
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path ='about' element={<About />}></Route>
+          
+
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
+  );
+}
+
+export default App;
