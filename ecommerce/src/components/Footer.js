b5ab5ebdd5f21ae057a8aeeb4089a1a5ebe6c4ee
Hrithik <ranjan.hrithik09@gmail.com>
2023-03-31 22:05:17 +0530

    Footer design completed

--- ecommerce/src/components/Footer.js
+++ ecommerce/src/components/Footer.js
@@ -1,11 +1,108 @@
-import React from 'react'
-
+import React from "react";
+import {Link} from "react-router-dom";
+import { BsLinkedin,BsGithub,BsYoutube,BsInstagram } from "react-icons/bs";
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img src="images/newsletter.png" alt="newsletter" />
+                <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-1"
+                  placeholder="Your email address"
+                  aria-label="Your email address"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div className="footer-link d-flex flex-column">
+                <address className="text-white fs-5">A-14, Keshav Laxmi Housing Society, <br />
+                Ideal Colony, Kothrud, Pune. <br />
+                Pin-411038</address>
+                <a href="+91 1234567890" className="text-white mt-3 d-block mb-1">+91 1234567890</a>
+                <a href="mailto:service@buyit.ac.in" className="text-white mt-2 d-block mb-0">service@buyit.ac.in</a>
+                <div className="social-icons d-flex align-items-center gap-30 mt-4">
+                  <a href="/" className="text-white">
+                    <BsLinkedin className=" fs-4"/>
+                  </a>
+                  <a href="/" className="text-white">
+                    <BsInstagram className=" fs-4"/>
+                  </a>
+                  <a href="/" className="text-white">
+                    <BsYoutube className=" fs-4"/>
+                  </a>
+                  <a href="/" className="text-white">
+                    <BsGithub className=" fs-4"/>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">Terms of Service</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Search</Link>
+                <Link className="text-white py-2 mb-1">About Us</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+                <Link className="text-white py-2 mb-1">Size chart</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Accessories</Link>
+                <Link className="text-white py-2 mb-1">Laptop</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+                <Link className="text-white py-2 mb-1">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()} | Powered by MAHA group{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
